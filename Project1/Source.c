e82a74305b0845f34926f6351dada364320bf2ec
shaaaaanxi <2883267@naver.com>
2018-05-31 17:56:54 +0900

    please

---
+++ Project1/Source.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+
+void multi(int i, int max, char*cal, int*num) {
+	int result = num[i] * num[i + 1];
+	num[i] = result;
+	for (int j = i; j < max - 1; j++) {
+		cal[j] = cal[j + 1];
+		num[j + 1] = num[j + 2];
+	}
+}
+
+void divide(int i, int max, char*cal, int*num) {
+	int result = num[i] / num[i + 1];
+	num[i] = result;
+	for (int j = i; j < max - 1; j++) {
+		cal[j] = cal[j + 1];
+		num[j + 1] = num[j + 2];
+	}
+}
+
+void plus(int i, int max, char*cal, int*num) {
+	int result = num[i] + num[i + 1];
+	num[i] = result;
+	for (int j = i; j < max - 1; j++) {
+		cal[j] = cal[j + 1];
+		num[j + 1] = num[j + 2];
+	}
+}
+
+void minus(int i, int max, char*cal, int*num) {
+	int result = num[i] - num[i + 1];
+	num[i] = result;
+	for (int j = i; j < max - 1; j++) {
+		cal[j] = cal[j + 1];
+		num[j + 1] = num[j + 2];
+	}
+}
+
+int main() {
+
+	char cal[10];
+	int num[10], i;
+	for (i = 0; cal[i - 1] != '='; i++) {
+
+		scanf_s("%d %c", &num[i], &cal[i]);
+		//scanf_s("%c", &cal[i], 1);
+		//printf("%c", cal[0]);
+	}
+	i--;
+	//printf("\n%d", i);
+	int max = i, result;
+	for (i = 0; i < max; i++) {
+		if (cal[i] == '*' || cal[i] == '/') {
+			if (cal[i] == '*') {
+				multi(i, max, cal, num);
+				max--;
+				i--;
+			}
+			else {
+				divide(i, max, cal, num);
+				max--;
+				i--;
+			}
+		}
+	}
+	for (i = 0; i < max; i++) {
+		if (cal[i] == '+' || cal[i] == '-') {
+			if (cal[i] == '+') {
+				plus(i, max, cal, num);
+				max--;
+				i--;
+			}
+			else{
+				minus(i, max, cal, num);
+				max--;
+				i--;
+			}
+		}
+	}
+	printf("%d\n", num[0]);
+
+	system("pause");
+}
